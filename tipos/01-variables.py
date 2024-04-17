badb6ad6d2149f051cf2894a18a17d9aa6ec69cb
elbarbero <elbarbero400@hotmail.com>
2024-04-17 21:12:55 +0200

    First lessons

---
+++ tipos/01-variables.py
@@ -0,0 +1,9 @@
+nombre_curso = "Ultimate Python" # variable tipo string
+nombre1 = "Hola" # variable tipo string
+NOMBRE_CURSO = "Mundo" # variable tipo string
+NoMbRe_CuRsO = "Chanchito" # variable tipo string
+NombreCurso = "Feliz" # variable tipo string
+print(nombre_curso, nombre1, NOMBRE_CURSO)
+alumnos = 5000 # variable tipo integer
+puntaje = 9.9 # variable tipo float
+publicado = True # variable tipo boolean
