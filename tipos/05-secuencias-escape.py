6a6fc764fa238b33ebf7c42d317042e3a15ffa1d
elbarbero <elbarbero400@hotmail.com>
2024-07-15 12:43:35 +0200

    COntinue with course of python

---
+++ tipos/05-secuencias-escape.py
@@ -0,0 +1,17 @@
+curso = "Ultimate Python"
+print(curso)
+
+curso = 'Ultimate "Python"'
+print(curso)
+
+curso = "Ultimate \"Python\""
+print(curso)
+
+curso = "Ultimate \\Python\""
+print(curso)
+
+curso = "Ultimate \n\\Python\""
+print(curso)
+
+curso = "Ultimate \nPython"
+print(curso)
